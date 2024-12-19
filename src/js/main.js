bf1316a1b0e4025924a7c12976a576226fa1b215
yousafqamar79 <yousafqamar79@yahoo.com>
2024-12-19 21:32:03 +0500

    Guidlines reference to canvas

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ class PhotoLab {
         this.templates = this.defineTemplates(); // Add this line
         this.initializeElements();
         this.setupEventListeners();
+        this.centeringGuidelines = null; // For centering guidelines
     }
 
     initializeElements() {
@@ -84,8 +85,298 @@ class PhotoLab {
         // Make canvas responsive
         this.makeCanvasResponsive();
         
+        this.initializeCenteringGuidelines(); // Add guidelines object to canvas
+//        this.initAligningGuidelines(); // Add object-to-object guidelines
         this.afterCanvasInit();
     }
+    
+//    initializeCenteringGuidelines() {
+//        if (!this.canvas) return;
+//
+//        const canvas = this.canvas;
+//        const ctx = canvas.getSelectionContext();
+//        const centerLineMargin = 4;
+//        const centerLineColor = "rgba(255,0,241,0.5)";
+//        const centerLineWidth = 1;
+//
+//        let viewportTransform = null;
+//        let isInVerticalCenter = false;
+//        let isInHorizontalCenter = false;
+//
+//        const canvasWidth = canvas.getWidth();
+//        const canvasHeight = canvas.getHeight();
+//        const canvasWidthCenter = canvasWidth / 2;
+//        const canvasHeightCenter = canvasHeight / 2;
+//
+//        const canvasWidthCenterMap = {};
+//        const canvasHeightCenterMap = {};
+//
+//        for (let i = canvasWidthCenter - centerLineMargin, len = canvasWidthCenter + centerLineMargin; i <= len; i++) {
+//            canvasWidthCenterMap[Math.round(i)] = true;
+//        }
+//        for (let i = canvasHeightCenter - centerLineMargin, len = canvasHeightCenter + centerLineMargin; i <= len; i++) {
+//            canvasHeightCenterMap[Math.round(i)] = true;
+//        }
+//
+//        const showCenterLine = (x1, y1, x2, y2) => {
+//            ctx.save();
+//            ctx.strokeStyle = centerLineColor;
+//            ctx.lineWidth = centerLineWidth;
+//            ctx.beginPath();
+//            ctx.moveTo(x1 * viewportTransform[0], y1 * viewportTransform[3]);
+//            ctx.lineTo(x2 * viewportTransform[0], y2 * viewportTransform[3]);
+//            ctx.stroke();
+//            ctx.restore();
+//        };
+//
+//        const showVerticalCenterLine = () => {
+//            showCenterLine(canvasWidthCenter + 0.5, 0, canvasWidthCenter + 0.5, canvasHeight);
+//        };
+//
+//        const showHorizontalCenterLine = () => {
+//            showCenterLine(0, canvasHeightCenter + 0.5, canvasWidth, canvasHeightCenter + 0.5);
+//        };
+//
+//        const onMouseDown = () => {
+//            viewportTransform = canvas.viewportTransform;
+//        };
+//
+//        const onObjectMoving = (e) => {
+//            
+//            const object = e.target;
+//            const objectCenter = object.getCenterPoint();
+//
+//            isInVerticalCenter = Math.round(objectCenter.x) in canvasWidthCenterMap;
+//            isInHorizontalCenter = Math.round(objectCenter.y) in canvasHeightCenterMap;
+//
+//            if (isInHorizontalCenter || isInVerticalCenter) {
+//                object.setPositionByOrigin(
+//                    new fabric.Point(
+//                        isInVerticalCenter ? canvasWidthCenter : objectCenter.x,
+//                        isInHorizontalCenter ? canvasHeightCenter : objectCenter.y
+//                    ),
+//                    'center',
+//                    'center'
+//                );
+//            }
+//        };
+//
+//        const onBeforeRender = () => {
+//            const ct = canvas.contextTop;
+//            if (ct) canvas.clearContext(ct);
+//        };
+//
+//        const onAfterRender = () => {
+//            console.log( 'isInVerticalCenter: ', isInVerticalCenter );
+//            if (isInVerticalCenter) showVerticalCenterLine();
+//            if (isInHorizontalCenter) showHorizontalCenterLine();
+//        };
+//
+//        const onMouseUp = () => {
+//            isInVerticalCenter = isInHorizontalCenter = null;
+//            canvas.renderAll();
+//        };
+//
+//        // Attach event listeners
+//        canvas.on("mouse:down", onMouseDown);
+//        canvas.on("object:moving", onObjectMoving);
+//        canvas.on("before:render", onBeforeRender);
+//        canvas.on("after:render", onAfterRender);
+//        canvas.on("mouse:up", onMouseUp);
+//    }
+    
+    initAligningGuidelines(canvas) {
+        var ctx = canvas.getSelectionContext(),
+            aligningLineOffset = 5,
+            aligningLineMargin = 4,
+            aligningLineWidth = 1,
+            aligningLineColor = 'rgb(0,255,0)',
+            viewportTransform,
+            zoom = 1;
+
+        function drawVerticalLine(coords) {
+            drawLine(
+                coords.x + 0.5,
+                coords.y1 > coords.y2 ? coords.y2 : coords.y1,
+                coords.x + 0.5,
+                coords.y2 > coords.y1 ? coords.y2 : coords.y1
+            );
+        }
+
+        function drawHorizontalLine(coords) {
+            drawLine(
+                coords.x1 > coords.x2 ? coords.x2 : coords.x1,
+                coords.y + 0.5,
+                coords.x2 > coords.x1 ? coords.x2 : coords.x1,
+                coords.y + 0.5
+            );
+        }
+
+        function drawLine(x1, y1, x2, y2) {
+            ctx.save();
+            ctx.lineWidth = aligningLineWidth;
+            ctx.strokeStyle = aligningLineColor;
+            ctx.beginPath();
+            ctx.moveTo(((x1 + viewportTransform[4]) * zoom), ((y1 + viewportTransform[5]) * zoom));
+            ctx.lineTo(((x2 + viewportTransform[4]) * zoom), ((y2 + viewportTransform[5]) * zoom));
+            ctx.stroke();
+            ctx.restore();
+        }
+
+        function isInRange(value1, value2) {
+            value1 = Math.round(value1);
+            value2 = Math.round(value2);
+            for (var i = value1 - aligningLineMargin, len = value1 + aligningLineMargin; i <= len; i++) {
+                if (i === value2) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        var verticalLines = [],
+            horizontalLines = [];
+
+        canvas.on('mouse:down', function () {
+            viewportTransform = canvas.viewportTransform;
+            zoom = canvas.getZoom();
+        });
+
+        canvas.on('object:moving', function (e) {
+            var activeObject = e.target,
+                canvasObjects = canvas.getObjects(),
+                activeObjectCenter = activeObject.getCenterPoint(),
+                activeObjectLeft = activeObjectCenter.x,
+                activeObjectTop = activeObjectCenter.y,
+                activeObjectBoundingRect = activeObject.getBoundingRect(),
+                activeObjectHeight = activeObjectBoundingRect.height / viewportTransform[3],
+                activeObjectWidth = activeObjectBoundingRect.width / viewportTransform[0],
+                horizontalInTheRange = false,
+                verticalInTheRange = false,
+                transform = canvas._currentTransform;
+
+            if (!transform) return;
+
+            for (var i = canvasObjects.length; i--;) {
+                if (canvasObjects[i] === activeObject) continue;
+
+                var objectCenter = canvasObjects[i].getCenterPoint(),
+                    objectLeft = objectCenter.x,
+                    objectTop = objectCenter.y,
+                    objectBoundingRect = canvasObjects[i].getBoundingRect(),
+                    objectHeight = objectBoundingRect.height / viewportTransform[3],
+                    objectWidth = objectBoundingRect.width / viewportTransform[0];
+
+                if (isInRange(objectLeft, activeObjectLeft)) {
+                    verticalInTheRange = true;
+                    verticalLines.push({
+                        x: objectLeft,
+                        y1: objectTop < activeObjectTop
+                            ? objectTop - objectHeight / 2 - aligningLineOffset
+                            : objectTop + objectHeight / 2 + aligningLineOffset,
+                        y2: activeObjectTop > objectTop
+                            ? activeObjectTop + activeObjectHeight / 2 + aligningLineOffset
+                            : activeObjectTop - activeObjectHeight / 2 - aligningLineOffset
+                    });
+                }
+
+                if (isInRange(objectLeft - objectWidth / 2, activeObjectLeft - activeObjectWidth / 2)) {
+                    verticalInTheRange = true;
+                    verticalLines.push({
+                        x: objectLeft - objectWidth / 2,
+                        y1: objectTop < activeObjectTop
+                            ? objectTop - objectHeight / 2 - aligningLineOffset
+                            : objectTop + objectHeight / 2 + aligningLineOffset,
+                        y2: activeObjectTop > objectTop
+                            ? activeObjectTop + activeObjectHeight / 2 + aligningLineOffset
+                            : activeObjectTop - activeObjectHeight / 2 - aligningLineOffset
+                    });
+                }
+
+                if (isInRange(objectLeft + objectWidth / 2, activeObjectLeft + activeObjectWidth / 2)) {
+                    verticalInTheRange = true;
+                    verticalLines.push({
+                        x: objectLeft + objectWidth / 2,
+                        y1: objectTop < activeObjectTop
+                            ? objectTop - objectHeight / 2 - aligningLineOffset
+                            : objectTop + objectHeight / 2 + aligningLineOffset,
+                        y2: activeObjectTop > objectTop
+                            ? activeObjectTop + activeObjectHeight / 2 + aligningLineOffset
+                            : activeObjectTop - activeObjectHeight / 2 - aligningLineOffset
+                    });
+                }
+
+                if (isInRange(objectTop, activeObjectTop)) {
+                    horizontalInTheRange = true;
+                    horizontalLines.push({
+                        y: objectTop,
+                        x1: objectLeft < activeObjectLeft
+                            ? objectLeft - objectWidth / 2 - aligningLineOffset
+                            : objectLeft + objectWidth / 2 + aligningLineOffset,
+                        x2: activeObjectLeft > objectLeft
+                            ? activeObjectLeft + activeObjectWidth / 2 + aligningLineOffset
+                            : activeObjectLeft - activeObjectWidth / 2 - aligningLineOffset
+                    });
+                }
+
+                if (isInRange(objectTop - objectHeight / 2, activeObjectTop - activeObjectHeight / 2)) {
+                    horizontalInTheRange = true;
+                    horizontalLines.push({
+                        y: objectTop - objectHeight / 2,
+                        x1: objectLeft < activeObjectLeft
+                            ? objectLeft - objectWidth / 2 - aligningLineOffset
+                            : objectLeft + objectWidth / 2 + aligningLineOffset,
+                        x2: activeObjectLeft > objectLeft
+                            ? activeObjectLeft + activeObjectWidth / 2 + aligningLineOffset
+                            : activeObjectLeft - activeObjectWidth / 2 - aligningLineOffset
+                    });
+                }
+
+                if (isInRange(objectTop + objectHeight / 2, activeObjectTop + activeObjectHeight / 2)) {
+                    horizontalInTheRange = true;
+                    horizontalLines.push({
+                        y: objectTop + objectHeight / 2,
+                        x1: objectLeft < activeObjectLeft
+                            ? objectLeft - objectWidth / 2 - aligningLineOffset
+                            : objectLeft + objectWidth / 2 + aligningLineOffset,
+                        x2: activeObjectLeft > objectLeft
+                            ? activeObjectLeft + activeObjectWidth / 2 + aligningLineOffset
+                            : activeObjectLeft - activeObjectWidth / 2 - aligningLineOffset
+                    });
+                }
+            }
+
+            if (!horizontalInTheRange) {
+                horizontalLines.length = 0;
+            }
+
+            if (!verticalInTheRange) {
+                verticalLines.length = 0;
+            }
+        });
+
+        canvas.on('before:render', function () {
+            const ct = canvas.contextTop;
+            if (ct) canvas.clearContext(ct);
+        });
+
+        canvas.on('after:render', function () {
+            for (var i = verticalLines.length; i--;) {
+                drawVerticalLine(verticalLines[i]);
+            }
+            for (var i = horizontalLines.length; i--;) {
+                drawHorizontalLine(horizontalLines[i]);
+            }
+
+            verticalLines.length = horizontalLines.length = 0;
+        });
+
+        canvas.on('mouse:up', function () {
+            verticalLines.length = horizontalLines.length = 0;
+            canvas.renderAll();
+        });
+    }
+
 
 //    applyTemplate(templateId) {
 //        if (!this.canvas) {
